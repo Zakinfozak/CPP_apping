47d06f97d9d24e7ad041a3b7ba3ed83e001f70cb
ZAK <zakiamine97@gmail.com>
2022-11-23 19:11:57 +0100

    selection sorte done !

--- selection_sort/my_sort.hpp
+++ selection_sort/my_sort.hpp
@@ -1,9 +1,25 @@
-// Change me instead
-// All occurences of XXX need to be replaced with
-// something more meaningful
 
-
-XXX
-void my_selection_sort( XXX begin, XXX end) {
-
+template <class T>
+void swap(T& x,T& y)
+{
+     T temp;
+     temp=x;
+     x=y;
+     y=temp;
+}
+template<class T>
+void my_selection_sort( T begin, T end)
+{
+    T min = begin;
+    for (T index = begin; index!= end; index++)
+    {
+        for (T bis = index ; bis != end; bis++)
+        {
+            if (*bis < *min)
+            {
+                min = bis;
+            }
+        }
+        swap(*min, *index);
+    }
 }
