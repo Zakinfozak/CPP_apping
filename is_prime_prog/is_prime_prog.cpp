786dfa3d81c7642d9c1ea313feaf9b3e280c7c30
ZAK <zakiamine97@gmail.com>
2022-11-23 20:01:13 +0100

    is_prime ok

--- is_prime_prog/is_prime_prog.cpp
+++ is_prime_prog/is_prime_prog.cpp
@@ -1,39 +1,31 @@
+
 #include <iostream>
 #include <string>
-
-
 using namespace std;
 long long int is_prime(long long int n) 
 {
-	
   bool is_prime = true;
-  
   if (n <= 1) {
     cout << n << " is a prime: False\n";
     return 0;
   }
 
-  for (long long int i = 2; i <= n / 2; ++i) {
+  for (long long int i = 2; i <= n/2; ++i) {
     if (n % i == 0) {
         cout << n << " is a prime: False\n";
         return 0;
     }
   }
-  
+
   cout << n << " is a prime: True\n";
   return 0;
 }
-
-
 int main(int argc, char *argv[])
 {
-    
-    long long int n;
-    
-    while (cin >> n) {
-        is_prime(n);
+    long long int i;
+    while (cin >> i) {
+        is_prime(i);
     }
     return 0;
 }
-
 
